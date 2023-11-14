86672efa0104485758e7e2db3f3c85dcac70866a
Harley Cardoso <harley.henrique@gmail.com>
2023-11-14 16:55:25 -0300

    Customizado App Bar e o Floating Action Button

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -11,63 +11,104 @@ class MoedasPage extends StatefulWidget {
 }
 
 class _MoedasPageState extends State<MoedasPage> {
-
   final tabela = MoedaRepository.tabela;
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
-  List<Object> selecionado = []; 
+  List<Object> selecionado = [];
 
   void _selectedItem(Moeda moeda) {
-    setState(() { 
-      (selecionado.contains(moeda) ? selecionado.remove(moeda) : selecionado.add(moeda));
+    setState(() {
+      (selecionado.contains(moeda)
+          ? selecionado.remove(moeda)
+          : selecionado.add(moeda));
     });
     print(selecionado.contains(moeda));
+  }
+
+  _appBarDinamico() {
+    if (selecionado.isEmpty) {
+      return AppBar(
+        centerTitle: true,
+        title: Text('Cripto Moedas'),
+        
+      );
+    } else {
+      return AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            setState(() {
+              selecionado = [];
+            });
+          },
+        ),
+        centerTitle: true,
+        title: Text('${selecionado.length} seleção'),
+        backgroundColor: Colors.blueGrey[50],
+        elevation: 1,
+        iconTheme: IconThemeData(color: Colors.black87),
+        titleTextStyle: TextStyle(
+            color: Colors.black87,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cripto Moedas'),
-        ),
-        body: ListView.separated(
-          itemBuilder: (BuildContext context, int moeda) {
-            return ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
+      appBar: _appBarDinamico(),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+            ),
+            leading: (selecionado.contains(tabela[moeda]))
+                ? CircleAvatar(
+                    child: Icon(Icons.check),
+                  )
+                : SizedBox(
+                    child: Image.asset(tabela[moeda].icone),
+                    width: 40,
+                  ),
+            title: Text(
+              tabela[moeda].nome,
+              style: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.w500,
+                color: Colors.indigo,
               ),
-              leading: (selecionado.contains(tabela[moeda]))
-              ? CircleAvatar(
-                child: Icon(Icons.check),
-              )
-              : SizedBox(
-                child: Image.asset(tabela[moeda].icone),
-                width: 40,
-              ),
-              title: Text(
-                tabela[moeda].nome,
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.indigo,
-                ),
-              ),
-              
-              trailing: Text(real.format(tabela[moeda].preco)),
-              selected: (selecionado.contains(tabela[moeda])),
-              selectedTileColor: Colors.indigo[50],
-              onLongPress: (){
-                _selectedItem(tabela[moeda]);
+            ),
+            trailing: Text(real.format(tabela[moeda].preco)),
+            selected: (selecionado.contains(tabela[moeda])),
+            selectedTileColor: Colors.indigo[50],
+            onLongPress: () {
+              _selectedItem(tabela[moeda]);
+            },
+          );
+        },
+        padding: EdgeInsets.all(16),
+        separatorBuilder: (_, __) => Divider(),
+        itemCount: tabela.length,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: (selecionado.isNotEmpty)
+          ? FloatingActionButton.extended(
+              onPressed: () {
+                selecionado = [];
+                _appBarDinamico();
               },
-              
-
-            );
-          },
-
-          padding: EdgeInsets.all(16),
-          separatorBuilder: (_, __) => Divider(),
-          itemCount: tabela.length,
-          
-
-        ));
+              icon: Icon(Icons.star),
+              label: Text(
+                'Favoritar',
+                style: TextStyle(
+                  letterSpacing: 0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ))
+          : null,
+    );
   }
 }
