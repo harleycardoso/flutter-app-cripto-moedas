4b6270402c1d2893af2f45c156379859a5d07212
Harley Cardoso <harley.henrique@gmail.com>
2023-11-14 15:00:46 -0300

    Selecinar item lista utilizando StateFulWidget

--- lib/pages/moedas_page.dart
+++ lib/pages/moedas_page.dart
@@ -1,14 +1,30 @@
-
 import 'package:flutter/material.dart';
+import 'package:joingroup/models/moeda.dart';
 import 'package:joingroup/repository/moeda_repository.dart';
+import 'package:intl/intl.dart';
 
-
-class MoedasPage extends StatelessWidget {
+class MoedasPage extends StatefulWidget {
   const MoedasPage({super.key});
 
   @override
+  State<MoedasPage> createState() => _MoedasPageState();
+}
+
+class _MoedasPageState extends State<MoedasPage> {
+
+  final tabela = MoedaRepository.tabela;
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  List<Object> selecionado = []; 
+
+  void _selectedItem(Moeda moeda) {
+    setState(() { 
+      (selecionado.contains(moeda) ? selecionado.remove(moeda) : selecionado.add(moeda));
+    });
+    print(selecionado.contains(moeda));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final tabela = MoedaRepository.tabela;
     return Scaffold(
         appBar: AppBar(
           title: Text('Cripto Moedas'),
@@ -16,14 +32,42 @@ class MoedasPage extends StatelessWidget {
         body: ListView.separated(
           itemBuilder: (BuildContext context, int moeda) {
             return ListTile(
-              leading: Image.asset(tabela[moeda].icone),
-              title: Text(tabela[moeda].nome),
-              trailing: Text(tabela[moeda].preco.toString()),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+              ),
+              leading: (selecionado.contains(tabela[moeda]))
+              ? CircleAvatar(
+                child: Icon(Icons.check),
+              )
+              : SizedBox(
+                child: Image.asset(tabela[moeda].icone),
+                width: 40,
+              ),
+              title: Text(
+                tabela[moeda].nome,
+                style: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.indigo,
+                ),
+              ),
+              
+              trailing: Text(real.format(tabela[moeda].preco)),
+              selected: (selecionado.contains(tabela[moeda])),
+              selectedTileColor: Colors.indigo[50],
+              onLongPress: (){
+                _selectedItem(tabela[moeda]);
+              },
+              
+
             );
           },
+
           padding: EdgeInsets.all(16),
           separatorBuilder: (_, __) => Divider(),
           itemCount: tabela.length,
+          
+
         ));
   }
 }
--- lib/repository/moeda_repository.dart
+++ lib/repository/moeda_repository.dart
@@ -3,40 +3,41 @@ import 'package:joingroup/models/moeda.dart';
 class MoedaRepository {
   static List<Moeda> tabela = [
     Moeda(
-      icone: 'images/1.png',
-      nome: 'Moeda1',
-      sigla: 'MED',
+      icone: 'images/0.png',
+      nome: 'Moeda0',
+      sigla: 'MED0',
       preco: 163803.00,
     ),
      Moeda(
       icone: 'images/1.png',
       nome: 'Moeda1',
-      sigla: 'MED',
-      preco: 163803.00,
+      sigla: 'MED1',
+      preco: 163803.01,
     ),
      Moeda(
-      icone: 'images/1.png',
-      nome: 'Moeda1',
-      sigla: 'MED',
-      preco: 163803.00,
+      icone: 'images/2.png',
+      nome: 'Moeda2',
+      sigla: 'MED2',
+      preco: 163803.02,
     ),
      Moeda(
-      icone: 'images/1.png',
-      nome: 'Moeda1',
-      sigla: 'MED',
-      preco: 163803.00,
+      icone: 'images/3.png',
+      nome: 'Moeda3',
+      sigla: 'MED3',
+      preco: 163803.03,
     ),
      Moeda(
-      icone: 'images/1.png',
-      nome: 'Moeda1',
-      sigla: 'MED',
-      preco: 163803.00,
+      icone: 'images/4.png',
+      nome: 'Moeda4',
+      sigla: 'MED4',
+      preco: 163803.04,
     ),
      Moeda(
-      icone: 'images/1.png',
-      nome: 'Moeda1',
-      sigla: 'MED',
-      preco: 163803.00,
+      icone: 'images/5.png',
+      nome: 'Moeda5',
+      sigla: 'MED5',
+      preco: 163803.05,
     ),
+    
   ];
 }
